72e45c90f6af9ad58cb46b4caa672cac6bc9ac7b
Diana-Verevkina <diva2208@mail.ru>
2022-10-13 01:47:32 +0300

    first

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Group, Post, Follow
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +20,22 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    following = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,6 +4,20 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(verbose_name='Название группы', max_length=200)
+    slug = models.SlugField(verbose_name='Уникальный адрес группы',
+                            unique=True)
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Сообщество'
+        verbose_name_plural = 'Сообщества'
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
@@ -11,6 +25,10 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE,
+        related_name='posts', blank=True, null=True
+    )
 
     def __str__(self):
         return self.text
@@ -24,3 +42,23 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь',
+                             on_delete=models.CASCADE, blank=True,
+                             null=True, related_name='follower',
+                             help_text='Ссылка на объект пользователя, '
+                                       'который подписывается')
+    following = models.ForeignKey(User, verbose_name='Автор',
+                               on_delete=models.CASCADE, blank=True,
+                               null=True, related_name='following',
+                               help_text='Ссылка на объект пользователя, '
+                                         'на которого подписываются')
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return self.author.username
